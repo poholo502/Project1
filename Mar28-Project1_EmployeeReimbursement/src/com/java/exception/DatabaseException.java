413186530fbd7fde78751c4cde6298d299aa9049
poholo502 <poholo502@gmail.com>
2019-04-12 12:49:45 -0500

    Remove

--- Mar28-Project1_EmployeeReimbursement/src/com/java/exception/DatabaseException.java
+++
@@ -1,9 +0,0 @@
-package com.java.exception;
-
-public class DatabaseException extends Exception{
-	public DatabaseException(String message) {
-		super(message);
-	}
-	//we are using this exception class so we can throw and catch our own exception
-	//it's good practice??
-}
